1663a17ac356f2c246c6a657c1857a3acafb212d
themrsgod <themrsgod@gmail.com>
2023-03-17 23:45:57 +0000

    assignment 1

---
+++ assignments/assignment_1.py
@@ -0,0 +1,19 @@
+"""
+Write a python script, declare 4 variables.
+1. two string variables that will take store name and store contact
+2. two lists that will take products and their prices respectively
+ """
+
+# Declaring variables
+storeName = "DHV"
+storeContact = "0241382366"
+products = ["amane", "koobi", "dawadawa", "momoni"]
+prices = [5.00, 3.00, 6.00, 1.00]
+
+print(f"Store Name: {storeName}")
+print(f"Store Contact: {storeContact}")
+print("======products======")
+print(products[0],prices[0])
+print(products[1],prices[2])
+print(products[2],prices[2])
+print(products[3],prices[3])
---
+++ main.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+
+# Declaring variables
+userName = "Vivian"
+userAge = 95
+userGender = "Female"
+items = ["banana", "chicken", "brush", "G-wagon", "amane"]
+
+print(userName)
+print(userAge)
+print(userGender)
+print(userGender[1])
+print(items[3])
+print(len(items))
